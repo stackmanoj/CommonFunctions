20e28f9939b599a6363cebb365eb81365da11cbd
Manoj <manojkumawat951@gmail.com>
2018-12-13 15:44:18 +0530

    Update only-number-directive.ts

--- only-number-directive.ts
+++ only-number-directive.ts
@@ -4,13 +4,21 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[OnlyNumber]'
 })
 export class OnlyNumber {
-  
+
   constructor(private el: ElementRef) { }
 
   @Input() OnlyNumber: boolean;
 
   @HostListener('input', ['$event'])
-    onInput(event: Event) {
-        this.el.nativeElement.value = (<HTMLInputElement>event.currentTarget).value.replace(/[^0-9]/g, '');
+  onInput(event: any) {
+    if (isNaN(parseInt(event.data))) {
+      event.preventDefault();
     }
+  }
+
+  @HostListener('paste', ['$event'])
+  onPaste(event: any) {
+    event.data = event.clipboardData.getData('Text');
+    this.onInput(event);
+  }
 }
