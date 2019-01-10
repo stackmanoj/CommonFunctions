ae4103caf85a591c1e3a7943ab43558af564dce5
Manoj <manojkumawat951@gmail.com>
2019-01-10 14:06:44 +0530

    Update only-number-directive.ts

--- only-number-directive.ts
+++ only-number-directive.ts
@@ -5,12 +5,25 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class OnlyNumber {
 
+  allowedKeys = [
+    'Backspace', 'ArrowLeft', 'ArrowRight', 'Escape', 'Tab', 'Delete', 'Home', 'End'
+  ];
+
   constructor(private el: ElementRef) { }
 
   @Input() OnlyNumber: boolean;
 
   @HostListener('keypress', ['$event'])
   onInput(event: any) {
+    let key: string = event.key;
+    let controlOrCommand = (event.ctrlKey === true || event.metaKey === true);
+    if (this.allowedKeys.indexOf(key) != -1 ||
+      (key == 'a' && controlOrCommand) ||
+      (key == 'c' && controlOrCommand) ||
+      (key == 'v' && controlOrCommand) ||
+      (key == 'x' && controlOrCommand)) {
+      return true;
+    }
     if (isNaN(parseInt(event.key))) {
       event.preventDefault();
     }
@@ -21,4 +34,6 @@ export class OnlyNumber {
     event.key = event.clipboardData.getData('Text');
     this.onInput(event);
   }
+
 }
+
